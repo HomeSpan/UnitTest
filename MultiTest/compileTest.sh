fa0e38d7f2bedb7c96f5a4b4ab3c9043b61a84e5
Gregg <homespan@icloud.com>
2025-11-11 09:40:23 -0600

    added gitignore to exclude log and build outputs

--- MultiTest/compileTest.sh
+++ MultiTest/compileTest.sh
@@ -13,7 +13,7 @@ core=$(arduino-cli core list | grep esp32)
 for board in ${boards[@]}; do
 
 	echo Compiling for $board under ${core}
-	arduino-cli compile -b esp32:esp32:$board --build-property build.partitions=min_spiffs --build-property upload.maximum_size=1966080 --warnings all --clean --export-binaries &> $board.out
+	arduino-cli compile -b esp32:esp32:$board --build-property build.partitions=min_spiffs --build-property upload.maximum_size=1966080 --warnings more --clean --export-binaries &> $board.out
 
 done
 
